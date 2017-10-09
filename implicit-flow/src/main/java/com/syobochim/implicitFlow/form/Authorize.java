8b04a93ff82d0b1238f3e1c877f206c71d91574e
syobochim <syobochim@gmail.com>
2017-10-09 22:48:50 +0900

    redirect uriは絶対パスで、フラグメントを持っていてはいけない(https://tools.ietf.org/html/rfc6749#section-3.1.2)

--- implicit-flow/src/main/java/com/syobochim/implicitFlow/form/Authorize.java
+++ implicit-flow/src/main/java/com/syobochim/implicitFlow/form/Authorize.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Required;
 
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author syobochim
@@ -12,8 +14,25 @@ public class Authorize {
     @NotNull
     public String responseType;
 
+    @NotNull
+    public String clientId;
+
+    public String redirectUri;
+
     @AssertTrue
     public boolean isToken() {
         return responseType.equals("token");
     }
+
+    @AssertTrue
+    public boolean isValidRedirectUri() {
+        URI uri;
+        try {
+            uri = new URI(redirectUri);
+        } catch (URISyntaxException e) {
+            return false;
+        }
+        return uri.isAbsolute() && uri.getFragment() == null;
+    }
+
 }
